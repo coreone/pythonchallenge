3729ed19e3d53e5e563191e5dd74c671e29aef13
Andrew Teixeira <teixeira@broadinstitute.org>
2017-09-14 16:12:57 -0400

    Add script to fetch the data for 2.py

---
+++ fetch2.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import requests
+import re
+
+url = "http://www.pythonchallenge.com/pc/def/ocr.html"
+
+response = requests.get(url)
+lines = response.content.split("\n")
+
+incomment = False
+indata = False
+numlines = 0
+data = []
+for line in lines:
+    if re.search("^<\!\-\-", line):
+        incomment = True
+        numlines = 1
+    elif re.search("^\-\->", line):
+        incomment = False
+        indata = True
+        numlines = 0
+    else:
+        if indata:
+            data.append(line)
+        else:
+            if incomment and numlines == 1:
+                if re.search("^%%\$", line):
+                    indata = True
+                    data.append(line)
+                    numlines += 1
+                else:
+                    numlines = 0
+
+for d in data:
+    if d:
+        print d
