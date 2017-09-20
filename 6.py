93d70409314b8d6ae031ae459bf4360ece2d939c
coreone <teixeira@broadinstitute.org>
2017-09-20 15:36:08 -0400

    more for 6

--- 6.py
+++ 6.py
@@ -1,9 +1,36 @@
 #!/usr/bin/env python
 
+import os
 import re
-import sys
+import requests
+import zipfile
 
 url = "http://www.pythonchallenge.com/pc/def/channel.html"
+
+
+def prep(webpage=None, filename=None, directory=None):
+    url = webpage.replace(".html", ".zip")
+    # url = "http://www.pythonchallenge.com/pc/def/channel.zip"
+
+    if webpage is None:
+        raise Exception("No webpage given to prep function.")
+    if filename is None:
+        raise Exception("No filename given to prep function.")
+    if directory is None:
+        raise Exception("No directory given to prep function.")
+
+    response = requests.get(url)
+
+    out = open(filename, "w")
+    out.write(response.content)
+    out.close()
+
+    if not os.path.isdir(directory):
+        os.mkdir(directory)
+
+    zip_ref = zipfile.ZipFile(filename, "r")
+    zip_ref.extractall(directory)
+    zip_ref.close()
 
 
 def solution(directory=None):
@@ -19,10 +46,15 @@ def solution(directory=None):
         if data:
             nothing = int(data.group(1))
         else:
+            print nothing
             return contents
             break
 
 
-data = solution("6")
+prep(url, "6.zip", "6")
 
-print data
+# for z in zip_ref.infolist():
+#     print z.comment
+# data = solution("6")
+#
+# print data
